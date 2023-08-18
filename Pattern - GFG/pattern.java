e6abfa76ee31d91727905571e0ffbd3282e426d6
Shah Nakshi <116349325+nakshishahh@users.noreply.github.com>
2023-08-18 11:14:55 +0530

    Added solution - LeetHub

---
+++ Pattern - GFG/pattern.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// Position this line where user code will be pasted.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+
+            Solution ob = new Solution();
+            ob.printDiamond(n);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+
+    void printDiamond(int n) {
+        for(int i = 0;i<n;i++){
+            for(int k =n-1 ;k>i;k--){
+                System.out.print(" ");
+            }
+            for(int j = 0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=n;i>0;i--){
+            for(int k = n-1;k>=i;k--){
+                System.out.print(" ");
+            }
+            for(int j = 0; j<i ; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        // Your code here
+    }
+}
